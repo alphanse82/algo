ee6676ceba9e2576b58b2218eb7a3720d97b6671
alphanse82 <RakeshKumarGupta@outlook.com>
2019-10-01 22:43:06 -0700

    OK

---
+++ InstallWUModules.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Unrestricted -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate
+Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -AddServiceFlag 7 -Confirm:$false
