1492d82eb8adab010f6fa3209af9c6fc82ea739c
alphanse82 <RakeshKumarGupta@outlook.com>
2019-10-02 14:22:37 -0700

    added log for WUManager setting

--- WindowsUpdate3.ps1
+++ WindowsUpdate3.ps1
@@ -14,12 +14,13 @@ if ( $false -eq (Test-Path $path)) {
 Set-ExecutionPolicy Unrestricted -Force
 
 Import-Module PSWindowsUpdate
-
+Get-WUServiceManager | Out-File "C:\WindowsUpdateList\Setting.log" -Append
 # adding Microsoft update to Server
 Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -AddServiceFlag 7 -Confirm:$false
 
 $retryCount = 0;
 
+Get-WUServiceManager | Out-File "C:\WindowsUpdateList\Setting.log" -Append
 Get-WUHistory | Out-File "C:\WindowsUpdateList\UpdateHistry_pre.log" -Force
 InstallWindowsUpdate $retryCount
 Get-WUHistory | Out-File "C:\WindowsUpdateList\UpdateHistry_post.log" -Force
